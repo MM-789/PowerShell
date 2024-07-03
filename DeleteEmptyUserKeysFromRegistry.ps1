fa90e6dda55094c6ba96743c8cfdbab19a1498c4
MM-789 <m.mayer@klinikum-weissenhof.de>
2024-07-03 16:21:25 +0200

    Update DeleteEmptyUserKeysFromRegistry.ps1
    -Recurse added

--- DeleteEmptyUserKeysFromRegistry.ps1
+++ DeleteEmptyUserKeysFromRegistry.ps1
@@ -4,7 +4,7 @@ $Userkeys = get-childitem -path $regpath | get-itemproperty | where-object -Prop
 
 $count = 0
 Foreach ($Key in $Userkeys) {
-    Remove-Item $Key.PSPath -WhatIf # -Whatif comment after a test run!
+    Remove-Item $Key.PSPath -Recurse -WhatIf # -Whatif comment after a test run!
     $count++
 }
 Write-Host $count . " keys deleted!"
